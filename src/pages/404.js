dcaa622837332f757e95863a88c5eae55f0fbc1d
Samo Kralj <samokralj95@gmail.com>
2021-02-11 15:24:01 +0100

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function HistoricEuleriansPage() {
+  return (
+    <Layout>
+      <h1> ERROR 404 - This page does not exist</h1>
+    </Layout>
+  )
+}
