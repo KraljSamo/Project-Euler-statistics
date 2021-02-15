fb9ef42933df22b724ab5fcfbf1e08bdaa3ab324
KraljSamo <samokralj95@gmail.com>
2021-02-15 13:24:57 +0100

    fixing uppercase filename issue

---
+++ src/components/layout.js
@@ -0,0 +1,26 @@
+import React from "react"
+import SideNavbar from "./side-navbar"
+import { Row, Col } from "reactstrap"
+import styles from "./layout.module.css"
+
+export default function Layout(props) {
+  return (
+    <body style={{ backgroundColor: "#1a1918", color: "orange", overflowX: "hidden" }}>
+      <Row>
+        <Col
+          md={2}
+          style={{
+            minHeight: "100vh",
+            borderRight: "solid #383838",
+            width: "100%",
+          }}
+        >
+          <SideNavbar />
+        </Col>
+        <Col className={styles.content} md={10}>
+          {props.children}
+        </Col>
+      </Row>
+    </body>
+  )
+}
