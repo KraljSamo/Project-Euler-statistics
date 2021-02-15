b7a79dd9650ad3fae53da3584f89a576d4ad25c5
KraljSamo <samokralj95@gmail.com>
2021-02-15 19:28:34 +0100

    some additional user data wip

--- python/inspect_data.py
+++ python/inspect_data.py
@@ -7,16 +7,26 @@ path = os.getcwd()
 with open(os.path.join(path, "..", "src", "data", "fastest_solvers.json"), "r") as infile:
     data = json.load(infile)
 
+with open(os.path.join(path, "..", "src", "data", "problems.json"), "r") as infile:
+    problem_data = json.load(infile)
+    problems = { problem["problem_num"] : problem for problem in problem_data}
+
 user_data = defaultdict(list)
 
 for index, problem in enumerate(data):
+    problem_num = problem["problem"]
     for entry in problem["fastest_solvers"]:
         user = entry["username"]
         place = entry["place"]
         user_data[user].append({
-            "problem" : problem["problem"],
-            "place" : place
+            "problem" : problem_num,
+            "place" : place,
+            "difficulty" : problems[problem_num]["difficulty"]
         })
+
+output = []
+for user, achievements in user_data.items():
+    pass
 
 with open(os.path.join(path, "..", "src", "data", "users.json"), "w") as outfile:
     output = [{ 
