14e23c1b953bc8972b8c399b99779db59422e355
KraljSamo <samokralj95@gmail.com>
2021-02-16 20:43:38 +0100

    styling side navbar

---
+++ src/pages/problems.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function ProblemsPage() {
+  return (
+    <Layout>
+      <h1>Page is under construction ... </h1>
+    </Layout>
+  )
+}
